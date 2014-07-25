2635a7fcf0f8186ecadb90ad687264683b226627
Chris Done <chrisdone@gmail.com>
2014-07-25 12:42:11 +0200

    Debug printing for connections and auto-kill threads

--- src/Server/Server.hs
+++ src/Server/Server.hs
@@ -4,10 +4,13 @@
 
 module Server.Server where
 
+import Data.IORef
 import Server.Client
 import Server.Commands
 import Server.Import
 import Server.Slave
+
+import System.IO
 
 -- | Start a server.
 startAccepter :: IO ()
@@ -16,11 +19,21 @@ startAccepter =
      server <- newServer main
      socket <- listenOn (PortNumber port)
      logger (Notice ("Listening on port " ++ show port ++ " ..."))
+     connections <- newIORef []
      finally (forever (do (h,host,remotePort) <- accept socket
-                          forkIO (startClient h host remotePort server)))
-             (do logger (Fatal "Server killed.")
-                 sClose socket)
-
+                          forkIO (do tid <- myThreadId
+                                     modifyIORef connections ((tid,h) :)
+                                     startClient h host remotePort server)))
+             (do cons <- readIORef connections
+                 logger (Fatal "Server killed!")
+                 mapM_ (\(tid,h) ->
+                          do logger (Debug ("Killing " ++ show tid ++ " ..."))
+                             killThread tid
+                             logger (Debug ("Killing connection " ++ show h ++ " ..."))
+                             hClose h)
+                       cons
+                 sClose socket
+                 logger (Debug ("Closed listener on port " ++ show port)))
   where port = 5233
 
 -- | Make a new server that will receive commands concurrently and
