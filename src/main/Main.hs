f78710c1c19e371ab1b954081b35531b7d8579ad
Chris Done <chrisdone@gmail.com>
2014-10-11 13:52:11 +0200

    Accept port in main

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -1,10 +1,18 @@
--- | GHC service
+-- | Main entry point to the program.
 
 module Main where
 
 import GHC.Server
 import GHC.Server.Logging
 
+import Data.Maybe
+import System.Environment
+
 -- | Main entry point.
 main :: IO ()
-main = runLogging (startServer 1990)
+main = do args <- fmap parse getArgs
+          runLogging (startServer (fromMaybe 5233 (lookup "port" args)))
+  where parse = go
+          where go ("--port":port:xs) = ("port",read port) : go xs
+                go (_:xs) = go xs
+                go [] = []
